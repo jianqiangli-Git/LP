c6dc3bc78e95fa33988cc56cb3e27d7b7756b7d2
= <=>
2018-12-07 17:03:11 +0800

    update Hn_AA_RA.py which implmented by modified lib_functions

--- Y_test.py
+++ Y_test.py
@@ -5,6 +5,58 @@ import random
 import numpy as np
 from copy import deepcopy
 import matplotlib.pyplot as plt
+from networkx.utils import not_implemented_for
+
+__all__ = ['resource_allocation_index',
+           'hn_resource_allocation_index',
+           'jaccard_coefficient',
+           'adamic_adar_index',
+           'hn_adamic_adar_index',
+           'preferential_attachment',
+           'cn_soundarajan_hopcroft',
+           'ra_index_soundarajan_hopcroft',
+           'within_inter_cluster']
+
+
+def _apply_prediction(G, func, ebunch=None):
+    if ebunch is None:
+        ebunch = nx.non_edges(G)
+    return ((u, v, func(u, v)) for u, v in ebunch)
+
+@not_implemented_for('multigraph')
+def resource_allocation_index(G, ebunch=None):
+    def predict(u, v):
+        return sum(1 / G.degree(w) for w in nx.common_neighbors(G, u, v))
+    return _apply_prediction(G, predict, ebunch)
+
+#注意这里因为除数不能为 0 的原因对某对节点的 Hn 指数为 0 的直接做了忽略处理，有些不妥，注意以后作出改进
+@not_implemented_for('multigraph')
+def hn_resource_allocation_index(G,Hn,ebunch=None):
+    def predict(u, v):
+        return sum(1 / Hn[w] for w in nx.common_neighbors(G, u, v) if Hn[w]!=0)
+    return _apply_prediction(G, predict, ebunch)
+
+@not_implemented_for('multigraph')
+def adamic_adar_index(G, ebunch=None):
+    def predict(u, v):
+        return sum(1 / math.log(G.degree(w)) for w in nx.common_neighbors(G, u, v))
+    return _apply_prediction(G, predict, ebunch)
+
+#注意这里因为除数不能为 0 的原因对某对节点的 Hn 指数为1 的直接做了忽略处理，有些不妥，注意以后作出改进
+@not_implemented_for('multigraph')
+def hn_adamic_adar_index(G,Hn, ebunch=None):
+    def predict(u, v):
+            return sum(1 / math.log(Hn[w]) for w in nx.common_neighbors(G, u, v) if Hn[w]!=1)
+    return _apply_prediction(G, predict, ebunch)
+
+def _community(G, u, community):
+    """Get the community of the given node."""
+    node_u = G.nodes[u]
+    try:
+        return node_u[community]
+    except KeyError:
+        raise nx.NetworkXAlgorithmError('No community information')
+
 
 #读取文件返回边的list集合
 def read_file(file_path):
@@ -171,6 +223,7 @@ def CN_num(adjMatrix,vertice,cur):
                 CN_info[(v,n)]=square[int(v)][int(n)]
     return CN_info
 
+#通过调用networkX库函数得到AA_RA指标，并存储到AA_info，RA_info字典中
 def AA_RA(G):
     AA_info={}
     RA_info={}
@@ -188,30 +241,63 @@ def AA_RA(G):
             RA_info[(i,j)]=k
     return AA_info,RA_info
 
-def Hindice_AA_RA(vertice,cur,adj,Hindice):
-    AA_info = {}
-    RA_info = {}
-    set_ver = set(vertice)
-    for v in vertice:
-        notLinked = set_ver-set(cur[v])
-        for n in notLinked:
-            if int(n)>int(v):
-                total_AA = 0
-                total_RA = 0
-                dup_vertice = deepcopy(vertice)
-                dup_vertice.remove(v)
-                for j in dup_vertice:
-                    if adj[int(v)][int(j)] !=0 and adj[int(j)][int(n)] != 0:
-                        k_j = sum(adj[int(j)])
-                        total_AA = total_AA+1/math.log(2,k_j)
-                        total_RA = total_RA+1/k_j
-                AA_info[(v,n)] = total_AA
-                RA_info[(v,n)] = total_RA
-                if (v,n) not in set(AA_info.keys()):
-                    AA_info[(v,n)] = 0
-                if (v, n) not in set(AA_info.keys()):
-                    AA_info[(v, n)] = 0
-    return AA_info,RA_info
+#通过修改库函数的AA_RA函数，假如Hn得到Hn各个指数的Hn_AA_RA指标，存到Hn_AA_info和Hn_RA_info字典中
+def getHn_AA_RA(Hn):
+    Hn_AA_info = {}
+    Hn_RA_info = {}
+    Hn_AA = {}
+    Hn_RA = {}
+    for i in Hn:
+        print("Get to calculate {i}_AA_RA... ".format(i=i))
+        pred_AA = hn_adamic_adar_index(G, Hn[i])
+        pred_RA = hn_resource_allocation_index(G, Hn[i])
+        for u, v, p in pred_AA:
+            if int(u)<int(v):
+                Hn_AA[(u,v)] = p
+            else:
+                Hn_AA[(v, u)] = p
+        for s, j, k in pred_RA:
+            if int(s) < int(j):
+                Hn_RA[(s, j)] = k
+            else:
+                Hn_RA[(j, s)] = k
+        Hn_RA_info[i] = Hn_RA
+        Hn_AA_info[i] = Hn_AA
+    return Hn_AA_info,Hn_RA_info
+
+
+# def Hindice_AA_RA(vertice,cur,adj,Hindice):
+#     Hn_AA_info = {}
+#     Hn_RA_info = {}
+#     AA_info = {}
+#     RA_info = {}
+#     set_ver = set(vertice)
+#     for h in Hindice:
+#         for v in vertice:
+#             notLinked = set_ver-set(cur[v])
+#             for n in notLinked:
+#                 if int(n)>int(v):
+#                     total_AA = 0
+#                     total_RA = 0
+#                     dup_vertice = deepcopy(vertice)
+#                     dup_vertice.remove(v)
+#                     for j in dup_vertice:
+#                         if adj[int(v)][int(j)] !=0 and adj[int(j)][int(n)] != 0:
+#                             Hn = Hindice[h][j]
+#                             if Hn==1:
+#                                 pass
+#                             else:
+#                                 total_AA = total_AA+1/math.log(Hn)
+#                                 total_RA = total_RA+1/Hn
+#                     AA_info[(v,n)] = total_AA
+#                     RA_info[(v,n)] = total_RA
+#                     if (v,n) not in set(AA_info.keys()):
+#                         AA_info[(v,n)] = 0
+#                     if (v, n) not in set(AA_info.keys()):
+#                         AA_info[(v, n)] = 0
+#         Hn_AA_info[h] = AA_info
+#         Hn_RA_info[h] = RA_info
+#     return Hn_AA_info,Hn_RA_info
 
 def LRW():
     pass
@@ -219,17 +305,37 @@ def LRW():
 if __name__ == "__main__":
     Yeast_path = r"C:\Users\Tang\Desktop\data\Yeast.txt"
     edges=read_file(Yeast_path)
-    G = nx.Graph()
     tr, te = devide_train_and_test_set(edges, 0.5)
     print("te:", te)
-    G.add_edges_from(tr)
-    nodes = set(G.nodes())
+    # G.add_edges_from(tr)
+    # nodes = set(G.nodes())
     v = set(getVertice(edges))
-    print("discard_nodes:", v - nodes)
-    G.add_nodes_from(v-nodes)
+    net = dictNet(tr, v)
+    G = nx.Graph(net)
+    # adj = createAdj(v, net)
+    # discard_node = v-nodes
+    # print("discard_nodes:", discard_node)
+    # G.add_nodes_from(discard_node)
+    degree = getDictDegree(G)
+    core = getCore(G)
     AA, RA = AA_RA(G)
     print("AUC_AA:",getAUC(te, AA))
+    print("AUC_RA:",getAUC(te, RA))
+    h = getHIndice(net, degree, core)
+    print("HnIndex", h)
+    # H_AA, H_RA = Hindice_AA_RA(v, net, adj, h)
+    H_AA,H_RA = getHn_AA_RA(h)
+    for i in H_AA:
+        print(i)
+        auc_AA = getAUC(te,H_AA[i])
+        print("AUC_AA_{index}:".format(index=i), auc_AA)
+        print("")
+    for j in H_RA:
+        print(j)
+        auc_RA = getAUC(te, H_RA[j])
+        print("AUC_RA_{index}:".format(index=j), auc_RA)
+        print("")
     plt.figure()
-    nx.draw(G, with_labels=True)
+    nx.draw(G,with_labels=True)
     plt.show()
 
