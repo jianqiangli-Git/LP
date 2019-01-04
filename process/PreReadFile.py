5846bc85ea287ef7389b3eab92fbf658ca2f2bf0
= <=>
2019-01-04 16:29:40 +0800

    complete a uniform interface for loading different net file

--- process/PreReadFile.py
+++ process/PreReadFile.py
@@ -1,16 +1,23 @@
 # 这个模块主要是对网络读入之前做检测，主要检测2个问题：
 # 1.节点是否从 0 开始
-# 2.是否是有向网络
+# 2.是否是有向网络(去重)
+# 最后根据检测结果提供读取文件的统一接口
 
-import process.linnkprediction as LP
-from copy import deepcopy
+import numpy as np
 
-# 传入边集得到节点的起始序号
-def StartVertice(edges):
-    v = LP.getVertice(edges)
-    v = sorted([int(i) for i in v])
-    return v[0]
+def standard(item):
+    if item[0] < item[1]:
+        return item
+    else:
+        return [item[1],item[0]]
 
-#决定舍去，没有必要检测,不管是有向还是无向图都可以直接转为无向图
-# def Direct(edges):
-#     pass
+# 读取文件部分
+def readFile(file_path):
+    f = np.loadtxt(file_path,dtype=int,usecols=(0,1))
+    f = np.array(list(map(standard,f)))
+    remove_dup = np.unique(f, axis=0)
+    start_v = np.min(remove_dup)
+    remove_dup = remove_dup - start_v
+    edge = np.ndarray.tolist(remove_dup)
+    edges = [[str(item[0]), str(item[1])] for item in edge]
+    return edges
